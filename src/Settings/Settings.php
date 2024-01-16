107cc43d9f0f0891f8cbdd99ee17d662fd13f135
Lucasarenaterry <lucasat2000@hotmail.com>
2024-01-16 16:27:00 +0000

    template for settings page created to add api key and placement id

--- src/Settings/Settings.php
+++ src/Settings/Settings.php
@@ -6,7 +6,9 @@ class Settings {
 
     public function setup() {
         add_action('admin_menu', array($this, 'add_adcaptcha_options_page'));
-        add_action( 'admin_enqueue_scripts', [ $this, 'add_styles_to_settings' ] );
+        add_action('admin_enqueue_scripts', [ $this, 'add_styles_to_settings' ]);
+        add_filter('admin_footer_text', array($this, 'change_admin_footer_text'), 11);
+        add_filter('update_footer', array($this, 'change_admin_footer_version'), 11);
     }
     
     public function add_adcaptcha_options_page() {
@@ -30,10 +32,25 @@ class Settings {
                 <?php printf('<img src="%s" class="logo"/>', esc_url('https://assets.adcaptcha.com/mail/logo_gradient.png')); ?>
                 <hr>
             </div>
-            <form method="post" action="options.php">
+            <div>
+            <div class="integrating-description">
+                <p>Before integrating, you must have an adCAPTCHA account and gone through the setup process. <a class="dashboard-link" href="https://app.adcaptcha.com/login" target="_blank">Dashboard &rarr;</a></p>
+            </div>
+            </div>
+            <form method="post" class="form" action="options.php">
                 <input type="text" id="api_key" class="api-key-input" name="adcaptcha_option_name[api_key]" value="" placeholder="API key">
+                <input type="text" id="placement_id" class="placement-id" name="adcaptcha_option_name[placement_id]" value="" placeholder="Placement ID">
+                <button type="submit" class="save-button">Save</button>
             </form>
         </div>
 		<?php
+    }
+
+    public function change_admin_footer_text() {
+        return '© 2024 AdCaptcha. All rights reserved.';
+    }
+
+    public function change_admin_footer_version() {
+        return 'Version 1.0.0';
     }
 }
