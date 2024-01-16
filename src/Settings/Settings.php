430f49750c1f75f844f7676485522982af4762ea
Lucasarenaterry <lucasat2000@hotmail.com>
2024-01-16 10:03:01 +0000

    Added styles to the settings page

--- src/Settings/Settings.php
+++ src/Settings/Settings.php
@@ -6,26 +6,34 @@ class Settings {
 
     public function setup() {
         add_action('admin_menu', array($this, 'add_adcaptcha_options_page'));
+        add_action( 'admin_enqueue_scripts', [ $this, 'add_styles_to_settings' ] );
     }
     
     public function add_adcaptcha_options_page() {
         add_options_page(
-            'AdCaptcha Settings',
+            'AdCaptcha',
             'AdCaptcha',
             'manage_options',
             'adcaptcha',
             array($this, 'render_adcaptcha_options_page')
         );
     }
+
+    public function add_styles_to_settings() {
+        wp_enqueue_style('adcaptcha-admin-styles', plugins_url('../styles/settings.css', __FILE__));
+    }
      
     public function render_adcaptcha_options_page() {
-        echo '<div class="wrap">';
-        echo '<h1>AdCaptcha Settings</h1>';
-        echo '<form method="post" action="options.php">';
-        settings_fields('adcaptcha_option_group');
-        do_settings_sections('adcaptcha');
-        submit_button();
-        echo '</form>';
-        echo '</div>';
+        ?>
+        <div class="admin">
+            <div class="header container">
+                <?php printf('<img src="%s" height="40px"/>', esc_url('https://assets.adcaptcha.com/mail/logo_gradient.png')); ?>
+                <hr>
+            </div>
+            <form method="post" action="options.php">
+                <input type="text" id="api_key" name="adcaptcha_option_name[api_key]" value="" placeholder="API key">
+            </form>
+        </div>
+		<?php
     }
 }
