6d4109910c021aa685e38f79541b4a07da96ca81
Lucasarenaterry <lucasat2000@hotmail.com>
2024-01-26 12:17:46 +0000

    Fixed woocommers forgot password

--- src/Plugin/PasswordReset.php
+++ src/Plugin/PasswordReset.php
@@ -9,11 +9,11 @@ use WP_Error;
 class PasswordReset {
 
     public function setup() {
-        global $wordpressPassword;
-        $wordpressPassword = $this;
+        global $wordpressPasswordReset;
+        $wordpressPasswordReset = $this;
         add_action( 'lostpassword_form', [ AdCaptcha::class, 'enqueue_scripts' ] );
         add_action( 'lostpassword_form', [ AdCaptcha::class, 'captcha_trigger' ] );
-        add_action( 'lostpassword_post', [ $wordpressPassword, 'verify' ], 10, 1 );
+        add_action( 'lostpassword_post', [ $wordpressPasswordReset, 'verify' ], 10, 1 );
     }
 
     public function verify( $errors ) {
--- src/Plugin/Woocommerce/PasswordReset.php
+++ src/Plugin/Woocommerce/PasswordReset.php
@@ -11,12 +11,16 @@ class PasswordReset {
     public function setup() {
         add_action( 'woocommerce_lostpassword_form', [ AdCaptcha::class, 'enqueue_scripts' ] );
         add_action( 'woocommerce_lostpassword_form', [ AdCaptcha::class, 'captcha_trigger' ] );
-        add_filter( 'lostpassword_post', [ $this, 'verify' ], 10, 1 );
+        add_action( 'wp_loaded', [ $this, 'remove_wp_action' ], 10 );
+        add_filter( 'allow_password_reset', [ $this, 'verify' ], 10, 1 );
+    }
+
+    public function remove_wp_action() {
+        global $wordpressPasswordReset;
+        remove_action( 'lostpassword_post', [ $wordpressPasswordReset, 'verify' ], 10 );
     }
 
     public function verify( $error ) {
-        global $wordpressPassword;
-        remove_action( 'lostpassword_post', [ $wordpressPassword, 'verify' ], 10 );
         $response = Verify::verify_token();
 
         if ( !$response ) {
