a9d373260acf6363bf269c055adf8120f4a90d66
Lucasarenaterry <lucasat2000@hotmail.com>
2024-03-08 12:59:16 +0100

    Added script to show captcha in form preview of Mailchimp

--- src/Plugin/Mailchimp/Forms.php
+++ src/Plugin/Mailchimp/Forms.php
@@ -11,6 +11,7 @@ class Forms {
 
     public function setup() {
         add_action( 'wp_enqueue_scripts', [ AdCaptcha::class, 'enqueue_scripts' ], 9 );
+        add_action( 'admin_enqueue_scripts', [ $this, 'form_preview_setup_triggers' ], 9 );
         add_filter( 'mc4wp_form_errors', [ $this, 'verify' ], 10, 2 );
         add_filter('mc4wp_form_messages', function($messages) {
             $messages = (array) $messages;
@@ -32,5 +33,16 @@ class Forms {
         }
 
         return  $errors;
+    }
+
+    public function form_preview_setup_triggers() {
+        $screen = get_current_screen();
+        if ($screen->id == 'mc4wp_page_mailchimp-for-wp-forms') {
+            echo '<script type="text/javascript">
+                setTimeout(() => {
+                        document.getElementById("mc4wp-form-content").addEventListener("change", () => { document.getElementById("mc4wp-form-preview").contentWindow.adcap.setupTriggers() }); 
+                }, 1000);
+            </script>';
+        }
     }
 }
