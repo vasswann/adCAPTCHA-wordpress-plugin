436c6d814f2b031c7e147af199b406d8b557cc42
Lucasarenaterry <lucasat2000@hotmail.com>
2024-03-08 17:59:04 +0100

    Removed incorrect commit

--- src/Widget/AdCaptcha.php
+++ src/Widget/AdCaptcha.php
@@ -4,7 +4,7 @@ namespace AdCaptcha\Widget\AdCaptcha;
 
 class AdCaptcha {
 
-    public static function enqueue_scripts($enableSubmitButton) {
+    public static function enqueue_scripts() {
         wp_enqueue_script('adcaptcha-script', 'https://widget.adcaptcha.com/index.js', array('jquery'), null, true);
     
         $ajax_nonce = wp_create_nonce("adcaptcha_nonce");
@@ -16,13 +16,13 @@ class AdCaptcha {
         echo '<script type="text/javascript">
             window.onload = function() {
                 if (window.adcap) {
-                    ' . self::setupScript($enableSubmitButton) . '
+                    ' . self::setupScript() . '
                 }
             }
         </script>';
     }
     
-    public static function setupScript($enableSubmitButton) {
+    public static function setupScript() {
         return 'window.adcap.init();
         window.adcap.setupTriggers({
             onComplete: () => {
@@ -35,16 +35,8 @@ class AdCaptcha {
                         nonce: adcaptcha_vars.nonce,
                     }
                 });
-                ' . ($enableSubmitButton ? self::enable_submit_button() : '') . '
             }
         });';
-    }
-
-    public static function enable_submit_button() {
-        return 'var submitButton = document.querySelector("#wp-submit");
-        if (submitButton) {
-            submitButton.disabled = false;
-        }';
     }
 
     public static function ob_captcha_trigger() {
