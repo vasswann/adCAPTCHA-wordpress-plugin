ea325658381405b9aebc04c7e103790b898ff28e
Lucasarenaterry <lucasat2000@hotmail.com>
2024-03-12 15:41:51 +0100

    Fixed errors from plugin checker

--- adcaptcha.php
+++ adcaptcha.php
@@ -7,7 +7,7 @@
  * Requires PHP: 7.4
  * Author: adCAPTCHA
  * Author URI: https://adcaptcha.com
- * Text Domain: adCAPTCHA-wordpress-plugin
+ * Text Domain: adcaptcha
  * Domain Path: /languages
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
