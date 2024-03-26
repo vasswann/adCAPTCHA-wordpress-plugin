e168788450be6241a0cbd781c8f13e9694d77cc5
Lucasarenaterry <lucasat2000@hotmail.com>
2024-03-26 11:45:15 +0000

    Moved general settings into a different file

---
+++ src/Settings/Plugins.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AdCaptcha\Settings\Plugins;
+
+class Plugins {
+     
+    public function render_Plugins_settings() {
+        ?>
+            <div>
+                <h1>Plugins</h1>
+                <p>adCAPTCHA is compatible with the following plugins:</p>
+                <ul>
+                    <li>WooCommerce</li>
+                    <li>Contact Form 7</li>
+                    <li>Mailchimp</li>
+                </ul>
+            </div>
+		<?php
+    }
+}
