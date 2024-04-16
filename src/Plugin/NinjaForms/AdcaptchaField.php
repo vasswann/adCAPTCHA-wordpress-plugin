a00488a88032c0d437bbeb1c38cfadc9b784c8b6
Lucasarenaterry <lucasat2000@hotmail.com>
2024-04-16 09:24:35 +0100

    Fixed bug for adcaptcha not being recognised by ninja forms

--- src/Plugin/NinjaForms/AdcaptchaField.php
+++ src/Plugin/NinjaForms/AdcaptchaField.php
@@ -4,53 +4,19 @@ namespace AdCaptcha\Plugin\NinjaForms\AdcaptchaField;
 use NF_Abstracts_Field;
 
 class AdcaptchaField extends NF_Abstracts_Field {
-    /**
-     * Name
-     *
-     * @var string
-     */
+
     protected $_name = 'adcaptcha';
 
-    /**
-     * Type
-     *
-     * @var string
-     */
     protected $_type = 'adcaptcha';
 
-    /**
-     * Template
-     *
-     * @var string
-     */
-    protected $_templates = 'checkbox';
+    protected $_templates = 'adcaptcha';
 
-    /**
-     * Section
-     *
-     * @var string
-     */
     protected $_section = 'misc';
 
-    /**
-     * Icon
-     *
-     * @var string
-     */
     protected $_icon = 'filter';
 
-    /**
-     * Nicename
-     *
-     * @var string
-     */
     protected $_nicename;
 
-    /**
-     * __construct
-     *
-     * @return void
-     */
     public function __construct() {
         parent::__construct();
         $this->_nicename = esc_html__( 'adCAPTCHA', 'adcaptcha' );
--- src/Plugin/NinjaForms/Forms.php
+++ src/Plugin/NinjaForms/Forms.php
@@ -3,6 +3,7 @@
 namespace AdCaptcha\Plugin\NinjaForms\Froms;
 
 use AdCaptcha\Plugin\NinjaForms\AdcaptchaField\AdcaptchaField;
+use AdCaptcha\Widget\AdCaptcha\AdCaptcha;
 use AdCaptcha\Plugin\Plugin;
 
 class Forms extends Plugin {
@@ -10,14 +11,32 @@ class Forms extends Plugin {
     public function setup() {
 		add_action('plugins_loaded', function() {
 			require_once plugin_dir_path(__FILE__) . '/AdcaptchaField.php';
+			add_action( 'wp_enqueue_scripts', [ AdCaptcha::class, 'enqueue_scripts' ]);
 			add_filter( 'ninja_forms_register_fields', [ $this, 'register_field' ] );
+			add_filter( 'ninja_forms_field_template_file_paths', [ $this, 'register_template' ]  );
+			add_filter( 'ninja_forms_localize_field_adcaptcha', [ $this, 'render_field' ] );
 		});
     }
 
 	public function register_field( $fields ): array {
 		$fields = (array) $fields;
-		$fields['adCAPTCHA'] = new AdcaptchaField();
+		$fields['adcaptcha'] = new AdcaptchaField();
 
 		return $fields;
+	}
+
+	public function register_template( $paths ): array {
+		$paths = (array) $paths;
+		$paths[] = __DIR__ . '/templates/';	
+
+		return $paths;
+	}
+
+	public function render_field( $field ): array {
+		$field = (array) $field;
+
+		$field['settings']['adcaptcha'] = AdCaptcha::ob_captcha_trigger();
+
+		return $field;
 	}
 }
