5589346afc1a48742610e8c7563a48f3f66d44cf
Lucasarenaterry <lucasat2000@hotmail.com>
2024-04-25 14:15:13 +0200

    Stop cf7 submission happening when no token

--- src/Plugin/ContactForm7/Forms.php
+++ src/Plugin/ContactForm7/Forms.php
@@ -9,21 +9,23 @@ class Forms {
 
     public function setup() {
         add_action( 'wp_enqueue_scripts', [ AdCaptcha::class, 'enqueue_scripts' ], 9 );
+        add_action( 'wp_enqueue_scripts', [ $this, 'block_submission' ], 9 );
         add_action( 'wp_enqueue_scripts', [ $this, 'reset_captcha_script' ], 9 );
         add_filter( 'wpcf7_form_elements', [ $this, 'captcha_trigger_filter' ], 20, 1 );
         add_filter( 'wpcf7_spam', [ $this, 'verify' ], 9, 1 );
     }
 
-    public function verify( $spam ) {
+    public function verify( $spam) {
         if ( $spam ) {
-			return $spam;
-		}
-
+            return $spam;
+        }
+    
         $verify = new Verify();
         $response = $verify->verify_token();
-
+    
         if ( $response === false ) {
             $spam = true;
+    
             add_filter('wpcf7_display_message', function($message, $status) {
                 if ($status == 'spam') {
                     $message = __( 'Incomplete captcha, Please try again.', 'adcaptcha' );
@@ -31,14 +33,14 @@ class Forms {
                 return $message;
             }, 10, 2);
         }
-
+    
         return $spam;
     }
 
     // Renders the captcha before the submit button
     public function captcha_trigger_filter(string $elements) {
         return preg_replace(
-            '/(<input.*?type="submit")/',
+            '/(<(input|button).*?type=(["\']?)submit(["\']?))/',
             AdCaptcha::ob_captcha_trigger() . '$1',
             $elements
             );
@@ -46,5 +48,27 @@ class Forms {
 
     public function reset_captcha_script() {
         wp_add_inline_script( 'adcaptcha-script', 'document.addEventListener("wpcf7mailsent", function(event) { ' . AdCaptcha::setupScript() . ' }, false);' );
+    }
+
+    public function block_submission() {
+        $script = '
+            document.addEventListener("DOMContentLoaded", function() {
+                var form = document.querySelector(".wpcf7-form");
+                if (form) {
+                var submitButton =[... document.querySelectorAll(".wpcf7 [type=\'submit\']")];
+                    if (submitButton) {
+                        submitButton.forEach(function(submitButton) {
+                            submitButton.addEventListener("click", function(event) {
+                                if (!window.adcap || !window.adcap.successToken) {
+                                    event.preventDefault();
+                                    return false;
+                                }
+                            });
+                        });
+                    }
+                }
+            });';
+    
+        wp_add_inline_script( 'adcaptcha-script', $script );
     }
 }
