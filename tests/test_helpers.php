80465a964d37b5a4dc1957987539d9d2107e428b
Istvan Vas <swannxxx@yahoo.co.uk>
2024-11-01 16:59:22 +0000

    Woo-commerce testing is done
    Small changes in the Login, PasswordReset and Registration php files. Verify added to the constructor for mock injection. Test for the free file is done. Namespace correcton.

--- tests/test_helpers.php
+++ tests/test_helpers.php
@@ -34,3 +34,10 @@ function execute_mocked_hook($hook_name) {
         }
     }
 }
+
+if(!function_exists('remove_action')) {
+    function remove_action($hook, $callback, $priority = 10, $accepted_args = 1) {
+        global $mocked_remove_actions;
+        $mocked_remove_actions[] = compact('hook', 'callback', 'priority', 'accepted_args');
+    }
+}
