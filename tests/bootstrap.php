3405a47c9fb6bc49ab049092ec8f7e89eb64772e
Istvan Vas <swannxxx@yahoo.co.uk>
2024-12-04 09:12:22 +0000

    Some setup changes for deprication

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,19 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\Brain\Monkey\setUp();
+// Suppress deprecations (temporary measure)
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 
-register_shutdown_function(function () {
-    \Brain\Monkey\tearDown();
-});
+// Set up Brain Monkey
+if (class_exists('\Brain\Monkey')) {
+    \Brain\Monkey\setUp();
+
+    // Ensure proper teardown on shutdown
+    register_shutdown_function(function () {
+        \Brain\Monkey\tearDown();
+    });
+}
+
+// Debugging (remove in production/testing CI once verified)
+var_dump(getcwd());
+var_dump(realpath(__DIR__ . '/../vendor/autoload.php'));
